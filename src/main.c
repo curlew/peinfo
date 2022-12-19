e1623fb9be893573712f3ad760c6626e833953a7
curlew <me@curlew.ch>
2022-12-19 19:08:10 +1000

    get headers

--- src/main.c
+++ src/main.c
@@ -1,9 +1,12 @@
 #include <stdio.h>
+#include <stdint.h>
 #include <windows.h>
 
 /*!
  * \retval  0 Success
  * \retval -1 Error opening file
+ * \retval -2 File is invalid (bad DOS header)
+ * \retval -3 File is invalid (bad PE header)
  */
 int wmain(int argc, wchar_t *argv[]) {
     if (argc != 2) {
@@ -39,8 +42,28 @@ int wmain(int argc, wchar_t *argv[]) {
         ret = -1;
         goto cleanup;
     }
-
     wprintf(L"Mapped view to %p\n", file_mapping_view);
+
+    PIMAGE_DOS_HEADER dos_header = (PIMAGE_DOS_HEADER)file_mapping_view;
+    if (dos_header->e_magic != IMAGE_DOS_SIGNATURE) {
+        fwprintf(stderr, L"Invalid DOS header (e_magic != MZ)\n");
+        ret = -2;
+        goto cleanup;
+    }
+    wprintf(L"Found valid DOS header\n");
+
+    // e_lfanew gives the offset of the PE header
+    PIMAGE_NT_HEADERS nt_headers = (PIMAGE_NT_HEADERS)((uint8_t *)dos_header + dos_header->e_lfanew);
+    if (nt_headers->Signature != IMAGE_NT_SIGNATURE) {
+        fwprintf(stderr, L"Invalid PE header (Signature != PE00)\n");
+        ret = -3;
+        goto cleanup;
+    }
+    wprintf(L"Found valid PE header\n");
+
+    PIMAGE_FILE_HEADER file_header = &nt_headers->FileHeader;
+    PIMAGE_OPTIONAL_HEADER optional_header = &nt_headers->OptionalHeader;
+
 
 cleanup:
     if (file_mapping_view != NULL)    { UnmapViewOfFile(file_mapping_view); }
