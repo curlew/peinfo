db7ec945c1edf700221c4200941fe9a7bfc41035
curlew <git@curlew.is>
2023-03-21 20:35:34 +1000

    use VirtualAlloc+ReadFile rather than CreateFileMapping+MapViewOfFile

--- src/main.c
+++ src/main.c
@@ -68,48 +68,47 @@ int wmain(int argc, wchar_t *argv[]) {
 
     int ret = 0;
     // variables defined here so cleanup label can be jumped to from anywhere
-    HANDLE file              = INVALID_HANDLE_VALUE,
-           file_mapping      = NULL; // CreateFileMappingW returns NULL on failure
-    LPVOID file_mapping_view = NULL;
+    HANDLE file      = INVALID_HANDLE_VALUE;
+    LPVOID file_base = NULL;
 
-    file = CreateFileW(file_path, GENERIC_READ, 0, NULL, OPEN_EXISTING, FILE_ATTRIBUTE_NORMAL, 0);
+    file = CreateFileW(file_path, GENERIC_READ, FILE_SHARE_READ, NULL, OPEN_EXISTING, FILE_ATTRIBUTE_NORMAL, NULL);
     if (file == INVALID_HANDLE_VALUE) {
         fwprintf(stderr, L"Error opening \"%s\"\n", file_path);
         ret = -1;
         goto cleanup;
     }
+    DWORD file_size = GetFileSize(file, NULL);
 
-    file_mapping = CreateFileMappingW(file, NULL, PAGE_READONLY, 0, 0, NULL);
-    if (file_mapping == NULL) {
-        fwprintf(stderr, L"Error creating file mapping\n");
+    file_base = VirtualAlloc(NULL, file_size, MEM_COMMIT, PAGE_READWRITE);
+    if (file_base == NULL) {
+        fputws(L"Error allocating space for file", stderr);
         ret = -1;
         goto cleanup;
     }
 
-    file_mapping_view = MapViewOfFile(file_mapping, FILE_MAP_READ, 0, 0, 0);
-    if (file_mapping_view == NULL) {
-        fwprintf(stderr, L"Error mapping view of file\n");
+    if (ReadFile(file, file_base, file_size, NULL, NULL) == FALSE) {
+        fputws(L"Error reading file", stderr);
         ret = -1;
         goto cleanup;
     }
-    wprintf(L"Mapped view to %p\n", file_mapping_view);
 
-    PIMAGE_DOS_HEADER dos_header = (PIMAGE_DOS_HEADER)file_mapping_view;
+    CloseHandle(file);
+
+
+    PIMAGE_DOS_HEADER dos_header = (PIMAGE_DOS_HEADER)file_base;
     if (dos_header->e_magic != IMAGE_DOS_SIGNATURE) {
-        fwprintf(stderr, L"Invalid DOS header (e_magic != MZ)\n");
+        fputws(L"Invalid DOS header (e_magic != MZ)", stderr);
         ret = -2;
         goto cleanup;
     }
-    wprintf(L"Found valid DOS header\n");
 
     // e_lfanew gives the offset of the PE header
     PIMAGE_NT_HEADERS nt_headers = (PIMAGE_NT_HEADERS)((uint8_t *)dos_header + dos_header->e_lfanew);
     if (nt_headers->Signature != IMAGE_NT_SIGNATURE) {
-        fwprintf(stderr, L"Invalid PE header (Signature != PE00)\n");
+        fputws(L"Invalid PE header (Signature != PE00)", stderr);
         ret = -3;
         goto cleanup;
     }
-    wprintf(L"Found valid PE header\n");
 
     wprintf(L"\n");
     if (opt_f) { print_file_header(&nt_headers->FileHeader); }
@@ -120,8 +119,7 @@ int wmain(int argc, wchar_t *argv[]) {
     }
 
 cleanup:
-    if (file_mapping_view != NULL)    { UnmapViewOfFile(file_mapping_view); }
-    if (file_mapping != NULL)         { CloseHandle(file_mapping); }
+    if (file_base != NULL)            { VirtualFree(file_base, 0, MEM_RELEASE); }
     if (file != INVALID_HANDLE_VALUE) { CloseHandle(file); }
     return ret;
 }
