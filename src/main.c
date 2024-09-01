be4c229ceda37188409f11f9660ba83617c8e6f9
curlew <git@curlew.is>
2024-09-01 16:04:34 +1000

    Move argument parsing out of wmain()

--- src/main.c
+++ src/main.c
@@ -3,24 +3,18 @@
 #include "optional_header.h"
 #include "section_table.h"
 #include <stdio.h>
+#include <stdlib.h>
 #include <stdint.h>
 #include <stdbool.h>
 #include <windows.h>
 
-void print_usage(wchar_t *exe) {
-    wprintf(L"Usage: %s [options] FILE\n", exe);
-    _putws(L"View headers and sections of Portable Executable files.\n"
-           L"\n"
-           L"Options:\n"
-           L"  -f  Display file header\n"
-           L"  -o  Display optional header\n"
-           L"  -s  Display section table\n"
-           L"  -i  Display import table\n"
-           L"  -e  Display export table\n"
-           L"  -h  Display this help message and exit\n"
-           L"\n"
-           L"If no options are specified, display all available information.");
-}
+typedef struct arguments {
+    bool f, o, s, i, e;
+    wchar_t *file;
+} arguments_t;
+
+static void print_usage(wchar_t *exe);
+static arguments_t parse_arguments(int argc, wchar_t *argv[]);
 
 /*!
  * \retval  0 Success
@@ -29,57 +23,16 @@ void print_usage(wchar_t *exe) {
  * \retval -3 File is invalid (bad PE header)
  */
 int wmain(int argc, wchar_t *argv[]) {
-    wchar_t *file_path = NULL;
-    bool opt_f = false, opt_o = false, opt_s = false, opt_i = false, opt_e = false;
-
-    for (int i = 1, operand_count = 0; i < argc; ++i) {
-        if (argv[i][0] == L'-') {
-            // option
-            for (wchar_t *c = argv[i] + 1; *c; ++c) {
-                switch (*c) {
-                case L'f': opt_f = true; break;
-                case L'o': opt_o = true; break;
-                case L's': opt_s = true; break;
-                case L'i': opt_i = true; break;
-                case L'e': opt_e = true; break;
-                case L'h':
-                    print_usage(argv[0]);
-                    return 0;
-                default:
-                    wprintf(L"Invalid option - \"%c\"\n\n", *c);
-                    print_usage(argv[0]);
-                    return 0;
-                }
-            }
-        } else {
-            // operand (non-option argument)
-            if (++operand_count > 1) {
-                print_usage(argv[0]);
-                return 0;
-            }
-            file_path = argv[i];
-        }
-    }
-
-    if (!file_path) {
-        wprintf(L"No file specified\n\n");
-        print_usage(argv[0]);
-        return 0;
-    }
-    if (!(opt_f || opt_o || opt_s || opt_i || opt_e)) {
-        // default options
-        opt_f = opt_o = opt_s = opt_i = opt_e = true;
-    }
-
+    arguments_t args = parse_arguments(argc, argv);
 
     int ret = 0;
     // variables defined here so cleanup label can be jumped to from anywhere
     HANDLE file      = INVALID_HANDLE_VALUE;
     LPVOID file_base = NULL;
 
-    file = CreateFileW(file_path, GENERIC_READ, FILE_SHARE_READ, NULL, OPEN_EXISTING, FILE_ATTRIBUTE_NORMAL, NULL);
+    file = CreateFileW(args.file, GENERIC_READ, FILE_SHARE_READ, NULL, OPEN_EXISTING, FILE_ATTRIBUTE_NORMAL, NULL);
     if (file == INVALID_HANDLE_VALUE) {
-        fwprintf(stderr, L"Error opening \"%s\"\n", file_path);
+        fwprintf(stderr, L"Error opening \"%s\"\n", args.file);
         ret = -1;
         goto cleanup;
     }
@@ -116,20 +69,20 @@ int wmain(int argc, wchar_t *argv[]) {
         goto cleanup;
     }
 
-    if (opt_f) {
+    if (args.f) {
         print_file_header(&nt_headers->FileHeader);
     }
-    if (opt_o) {
+    if (args.o) {
         print_optional_header(&nt_headers->OptionalHeader);
     }
-    if (opt_s) {
+    if (args.s) {
         print_section_table(IMAGE_FIRST_SECTION(nt_headers),
                             nt_headers->FileHeader.NumberOfSections);
     }
-    if (opt_i) {
+    if (args.i) {
         print_import_table(file_base);
     }
-    if (opt_e) {
+    if (args.e) {
         print_export_table(file_base);
     }
 
@@ -137,4 +90,64 @@ cleanup:
     if (file_base != NULL)            { VirtualFree(file_base, 0, MEM_RELEASE); }
     if (file != INVALID_HANDLE_VALUE) { CloseHandle(file); }
     return ret;
+}
+
+void print_usage(wchar_t *exe) {
+    wprintf(L"Usage: %s [options] FILE\n", exe);
+    _putws(L"View headers and sections of Portable Executable files.\n"
+           L"\n"
+           L"Options:\n"
+           L"  -f  Display file header\n"
+           L"  -o  Display optional header\n"
+           L"  -s  Display section table\n"
+           L"  -i  Display import table\n"
+           L"  -e  Display export table\n"
+           L"  -h  Display this help message and exit\n"
+           L"\n"
+           L"If no options are specified, display all available information.");
+}
+
+arguments_t parse_arguments(int argc, wchar_t *argv[]) {
+    arguments_t args = {0};
+
+    for (int i = 1, operand_count = 0; i < argc; ++i) {
+        if (argv[i][0] == L'-') {
+            // option
+            for (wchar_t *c = argv[i] + 1; *c; ++c) {
+                switch (*c) {
+                case L'f': args.f = true; break;
+                case L'o': args.o = true; break;
+                case L's': args.s = true; break;
+                case L'i': args.i = true; break;
+                case L'e': args.e = true; break;
+                case L'h':
+                    print_usage(argv[0]);
+                    exit(0);
+                default:
+                    wprintf(L"Invalid option - \"%c\"\n\n", *c);
+                    print_usage(argv[0]);
+                    exit(0);
+                }
+            }
+        } else {
+            // operand (non-option argument)
+            if (++operand_count > 1) {
+                print_usage(argv[0]);
+                exit(0);
+            }
+            args.file = argv[i];
+        }
+    }
+
+    if (!args.file) {
+        wprintf(L"No file specified\n\n");
+        print_usage(argv[0]);
+        exit(0);
+    }
+    if (!(args.f || args.o || args.s || args.i || args.e)) {
+        // default options
+        args.f = args.o = args.s = args.i = args.e = true;
+    }
+
+    return args;
 }
